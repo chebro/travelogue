e97273a1ad3ee3406b29a3e76a4a36afff11aa61
Nirhar <manishkausikhari@gmail.com>
2021-12-18 20:04:45 +0530

    Map loading of journeys is done, addition of new locations is yet to be done

--- server/src/client/user/assets/js/maps.js
+++ server/src/client/user/assets/js/maps.js
@@ -1,25 +1,35 @@
 // Initialize and add the map
 
-markers=[];
-
+var coords=[];
+var map=null;
 function initMap(latitude, longitude, zoom) {
     // The location of Uluru
     const uluru = { lat: latitude, lng: longitude };
     // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
+    const m = new google.maps.Map(document.getElementById("map"), {
         zoom: zoom,
         center: uluru,
     });
+    map=m;
     console.log("Map Initialised");
 }
 
-function add_marker(latitude,longitude,url){
+function add_marker(latitude,longitude){
     const uluru = { lat: latitude, lng: longitude };
     const marker = new google.maps.Marker({
         position: uluru,
         map: map,
     });
-    markers
+    coords.push(uluru);
+    /*Google Map Marker Click Function*/
+    google.maps.event.addListener(marker, 'click', (function(marker) {
+        return function() {
+            //Query text for 
+            $("#display_modal_title").text("Sample Modal");
+            $("#display_modal_body").text("Lorem Ipsum");
+            $('#display_modal').modal('show');
+        }
+    })(marker));
 }
 
 function setup_travel(){
@@ -27,14 +37,22 @@ function setup_travel(){
 
     //
     initMap(-25.344,131.036,4);
+    add_marker(-25.344,131.036);
+    add_marker(-24.344,130.036);
+    add_marker(-20.344,130.036);
+
+    const flightPath = new google.maps.Polyline({
+        path: coords,
+        geodesic: true,
+        strokeColor: "#FF0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+      });
+
+    flightPath.setMap(map);
+    var bounds = new google.maps.LatLngBounds();
+    bounds.extend(coords[0]);
+    bounds.extend(coords[coords.length-1]);
+    map.fitBounds(bounds);
 }
 
-/*Google Map Marker Click Function*/
-google.maps.event.addListener(marker, 'click', (function(marker) {
-    return function() {
-    /*Bootstrap Modal Pop Up Open Code*/
-    $(".modal-title").text("This is google map..");
-    $(".modal-body").text("Modal Body");
-    $("#myModal").modal('show');
-    }
-})(marker));
