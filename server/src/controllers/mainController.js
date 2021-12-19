dbfe8d1a773142af5fb26d940ef6f2bae5051770
Nirhar <manishkausikhari@gmail.com>
2021-12-19 08:50:44 +0530

    Added frontend for adding new journeys

--- server/src/controllers/mainController.js
+++ server/src/controllers/mainController.js
@@ -17,12 +17,14 @@ exports.getTravels = wrapAsync(async (req, res) => {
 	const name = req.params.user
 	const user = await User.findOne({ name })
 	let isPublished = []
-
+	let journeyTitles=[]
 	for(let i = 0; i < user.journeys.length; i++) {
-		isPublished.push(user.journeys[i].published)	
+		isPublished.push(user.journeys[i].published)
+		journeyTitles.push(user.journeys[i].title)	
 	}
 	const inject = {
 		name,
+		title: journeyTitles,
 		num_cards: user.journeys.length,
 		profile_pic: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
 		port_image: 'https://travel.usnews.com/dims4/USNEWS/a600cb2/2147483647/resize/445x280%5E%3E/crop/445x280/quality/85/?url=https://travel.usnews.com/images/tahiti_main_getty_samantha_t_photography_edited_445x280_v43QKbF.jpg',
